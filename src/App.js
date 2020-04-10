55aa2c49674dff7e4402195eac8235e4f44ba873
pedrivas <42252328+pedrivas@users.noreply.github.com>
2020-04-09 23:31:36 -0300

    solucao desafio 03

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
+
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepos(response.data);
+    })
+  }, [] );
+
+  async function handleAddRepository(e) {
+    e.preventDefault();
+
+    const data = {
+      "title":`Meu Projeto ${Date.now()}`,
+      "url":"https://github.com/pedrivas/go-stack-desafio-03-reactjs",
+      "techs":["React, React Native, Imsomnia, NodeJS"]
+    }
+
+    try {
+        const response = await api.post('repositories', data);
+        setRepos([ ...repos, response.data])
+    } catch (err) {
+        alert(`Erro no cadastro. Tente novamente.`)
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    setRepos(repos.filter(
+      repo => repo.id !== id
+    ))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+          {repos.map(repo => (
+            <li key={repo.id}>
+              {repo.title}
+              <button onClick={() => handleRemoveRepository(repo.id)}>
+                Remover
+              </button>
+            </li>
+          ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
